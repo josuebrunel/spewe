f22ba387acc20de64d06e6ff4b8a228e1aeab888
Josue Kouka <jkouka@entrouvert.com>
2017-04-10 17:51:09 +0200

    add Request object

--- spewe/__init__.py
+++ spewe/__init__.py
@@ -17,6 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# import cgi
 
 
 class Spewe(object):
@@ -24,5 +25,45 @@ class Spewe(object):
     def __call__(self, env, start_response):
         status = '200 OK'
         headers = {'Content-Type': 'text/plain'}
+        # import pdb; pdb.set_trace()
+        print(Request(env))
         start_response(status, headers.items())
         return ['Hello world!\n']
+
+
+class Request(object):
+
+    def __init__(self, env):
+        self._environ = env
+        self.method = env['REQUEST_METHOD']
+        self.path = env['PATH_INFO']
+        self.query_string = env['QUERY_STRING']
+        self.content_type = env['CONTENT_TYPE']
+        self.content_length = env['CONTENT_LENGTH']
+        self.server_name = env['SERVER_NAME']
+        self.server_port = env['SERVER_PORT']
+        self.server_protocol = env['SERVER_PROTOCOL']
+        self.remote_address = env['REMOTE_ADDR']
+        self.remote_host = env['REMOTE_HOST']
+        # self.body = env['wsgi.input'].read()
+        # if 'wsgi.file_wrapper':
+        #     self.form = cgi.FieldStorage(fp=env['wsgi.input'], environ=env)
+        self._wsgi = {key: value for key, value in env.items() if key.startswith('wsgi')}
+        self.headers = {key: value for key, value in env.items() if key.startswith('HTTP')}
+
+    def __str__(self):
+        return '%s - %s' % (self.method, self.get_full_path())
+
+    __repr__ = __str__
+
+    def get_full_path(self):
+        return '%s%s' % (self.server_name, self.path)
+
+    def build_absolute_uri(self, path):
+        return '%s%s' % (self.server_name, path)
+
+
+class Response(object):
+
+    def __init__(self, env):
+        pass

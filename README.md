38c67d2a9b8675af386e1b913089c8b3061b1583
Josue Kouka <jkouka@entrouvert.com>
2017-04-10 15:12:32 +0200

    setup project

---
+++ README.md
@@ -0,0 +1,6 @@
+SPEWE
+=====
+
+Spewe is little Python Web Framework.
+Just like spew, this web framework isn't to be used anywhere. Its development is purely educational.
+Therefore no need to expect anything Django fancy or whatsoever
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+__author__ = 'Josue Kouka'
+__email__ = 'josuebrunel@gmail.com'
+__version__ = '0.1'
+
+#   requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="spewe",
+    version=__version__,
+    description="Python Yahoo OAuth Library. Supports OAuth1 and OAuth2",
+    long_description=read("README.rst"),
+    author=__author__,
+    author_email=__email__,
+    url="https://github.com/josuebrunel/spewe",
+    download_url="https://github.com/josuebrunel/spewe/archive/{0}.tar.gz".format(__version__),
+    keywords=['web', 'framework', 'wsgi'],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ],
+    platforms=['Any'],
+    license='MIT',
+    install_requires=required
+)
